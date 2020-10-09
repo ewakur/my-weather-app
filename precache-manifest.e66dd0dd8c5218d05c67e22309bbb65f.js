b46c12288560b1a286298ea9bcf8c82ea9906b80
ewakur <ewa.kurczab13@gmail.com>
2020-10-09 21:31:06 +0200

    Updates

---
+++ precache-manifest.e66dd0dd8c5218d05c67e22309bbb65f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8caf4d6cbaea56ea33e",
+    "url": "/my-weather-app/static/css/main.5b3d4df5.chunk.css"
+  },
+  {
+    "revision": "f8caf4d6cbaea56ea33e",
+    "url": "/my-weather-app/static/js/main.4580a80d.chunk.js"
+  },
+  {
+    "revision": "53cb4d92ee16cf7ab6a6",
+    "url": "/my-weather-app/static/js/runtime~main.357c23fc.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/my-weather-app/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "4406e267e0a96f4a54aecb262471457a",
+    "url": "/my-weather-app/static/media/clouds.4406e267.jpg"
+  },
+  {
+    "revision": "17f7f66dac7b8ab81fdedff9a56f6eac",
+    "url": "/my-weather-app/index.html"
+  }
+];
